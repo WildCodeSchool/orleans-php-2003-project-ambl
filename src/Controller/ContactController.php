95de16aeb786b9903ac0c4fc502813059842ec25
kodev-sudo <k.ollivierdev@gmail.com>
2020-04-21 12:41:36 +0200

    adding some PHP form checking

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -23,4 +23,36 @@ class ContactController extends AbstractController
     {
         return $this->twig->render('Contact/contact.html.twig');
     }
+
+    public function send()
+    {
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST')
+        {
+            $contactData['contact_firstname'] = trim($_POST['contact_firstname']);
+            $contactData['contact_lastname'] = trim($_POST['contact_lastname']);
+            $contactData['contact_email'] = trim($_POST['contact_email']);
+            $contactData['contact_phonenumber'] = trim($_POST['contact_phonenumber']);
+
+            $contactErrors = [];
+            if (empty($_POST['contact_firstname'])) {
+                $contactErrors[] = 'Un prénom est requis';
+            }
+            if (empty($_POST['contact_lastname'])) {
+                $contactErrors[] = 'Un nom est requis';
+            }
+            if (empty($_POST['contact_email'])) {
+                $contactErrors[] = 'Un email est requis';
+            }
+            if (!filter_var($_POST['contact_email'], FILTER_VALIDATE_EMAIL)) {
+                $contactErrors[] = 'Le format d\'email est invalide';
+            }
+            if (empty($_POST['contact_phonenumber'])) {
+                $contactErrors[] = 'Un numéro de téléphone est requis';
+            }
+            if (empty($_POST['contact_message'])) {
+                $contactErrors[] = 'Votre message ne peut être  vide';
+            }
+        }
+    }
 }
