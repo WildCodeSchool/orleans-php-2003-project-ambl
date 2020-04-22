0e5f466d344ebeae7f359d6d398ce9e1070690c3
kodev-sudo <k.ollivierdev@gmail.com>
2020-04-22 13:31:31 +0200

    Adding checking for form, show errors and success messages

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -24,35 +24,46 @@ class ContactController extends AbstractController
         return $this->twig->render('Contact/contact.html.twig');
     }
 
-    public function send()
+    public function checkingForm()
     {
-
-        if($_SERVER['REQUEST_METHOD'] === 'POST')
-        {
-            $contactData['contact_firstname'] = trim($_POST['contact_firstname']);
-            $contactData['contact_lastname'] = trim($_POST['contact_lastname']);
-            $contactData['contact_email'] = trim($_POST['contact_email']);
-            $contactData['contact_phonenumber'] = trim($_POST['contact_phonenumber']);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $contactData = array_map('trim', $_POST);
 
             $contactErrors = [];
-            if (empty($_POST['contact_firstname'])) {
+            $contactSuccess = '';
+            if (empty($contactData['contact_firstname'])) {
                 $contactErrors[] = 'Un prénom est requis';
             }
-            if (empty($_POST['contact_lastname'])) {
+            if (empty($contactData['contact_lastname'])) {
                 $contactErrors[] = 'Un nom est requis';
             }
-            if (empty($_POST['contact_email'])) {
+            if (empty($contactData['contact_email'])) {
                 $contactErrors[] = 'Un email est requis';
             }
-            if (!filter_var($_POST['contact_email'], FILTER_VALIDATE_EMAIL)) {
+            if (!filter_var($contactData['contact_email'], FILTER_VALIDATE_EMAIL)) {
                 $contactErrors[] = 'Le format d\'email est invalide';
             }
-            if (empty($_POST['contact_phonenumber'])) {
+            if (empty($contactData['contact_phonenumber'])) {
                 $contactErrors[] = 'Un numéro de téléphone est requis';
             }
-            if (empty($_POST['contact_message'])) {
-                $contactErrors[] = 'Votre message ne peut être  vide';
+            //if (!filter_var($contactData['contact_phonenumber'], FILTER_VALIDATE_INT)) {
+            //    $contactErrors[] = 'Le numéro de téléphone doit être composé de chiffres uniquement';
+            //}
+            if (strlen($contactData['contact_phonenumber']) > 10) {
+                $contactErrors[] = 'Le numéro de téléphone doit être composé de dix chiffres seulement';
             }
+            if (strlen($contactData['contact_phonenumber']) < 10) {
+                $contactErrors[] = 'Le numéro de téléphone doit être composé de 10 chiffres';
+            }
+            if (empty($contactData['contact_message'])) {
+                $contactErrors[] = 'Votre message ne peut être  vide';
+            } else {
+                $contactSuccess = "Bonjour " . htmlentities($contactData['contact_firstname']) . " " . htmlentities($contactData['contact_lastname']) . ", votre message a bien été envoyé. Merci.";
+
+            }
+            var_dump($_POST);
+            return $this->twig->render('Contact/contact.html.twig',['contactErrors'=>$contactErrors,'contactSuccess'=>$contactSuccess]);
         }
+
     }
 }
