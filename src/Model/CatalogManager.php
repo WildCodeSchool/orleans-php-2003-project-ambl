b84e4846acb2a1c5b329e433f455f3dd7b3d3b36
dante18 <lablandinerie2@gmail.com>
2020-04-22 18:37:34 +0200

    Correction of PDO errors in CatalogManager.php

--- src/Model/CatalogManager.php
+++ src/Model/CatalogManager.php
@@ -38,10 +38,10 @@ class CatalogManager extends AbstractManager
     public function selectAllByType(string $type = 'mushroom'): array
     {
         $query = 'SELECT element.name, element.picture FROM ' . $this->table . ' JOIN type ON ' . $this->table;
-        $query .= '.type_id = type.id WHERE type.name = "' . $type . '" LIMIT ' . self::MAX_RESULT;
+        $query .= '.type_id = type.id WHERE type.name = ":type" LIMIT ' . self::MAX_RESULT;
 
         $statement = $this->pdo->prepare($query);
-        $statement->bindValue('type', $type, \PDO::PARAM_STR);
+        $statement->bindValue(':type', $type, \PDO::PARAM_STR);
         $statement->execute();
 
         return $statement->fetchAll();
