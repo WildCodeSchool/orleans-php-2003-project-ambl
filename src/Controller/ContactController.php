a156c4994d7551c2aa43f3631694388bf12de24a
kodev-sudo <k.ollivierdev@gmail.com>
2020-04-23 16:51:35 +0200

    Last form checks

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -19,46 +19,66 @@ class ContactController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function index()
+    public function index(string $contactSuccess = '')
     {
-        return $this->twig->render('Contact/contact.html.twig');
+        if ($contactSuccess == 'success') {
+            $message = "Votre message a bien été envoyé. Merci.";
+        } else {
+            $message = '';
+        }
+        return $this->twig->render('Contact/index.html.twig', ['contactSuccess' => $message]);
     }
 
+    /**
+     * Method required to check form
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
     public function checkingForm()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $contactData = array_map('trim', $_POST);
+            $contactData['contact_firstname'] = ucfirst($contactData['contact_firstname']);
+            $contactData['contact_lastname'] = ucfirst($contactData['contact_lastname']);
 
             $contactErrors = [];
             $contactSuccess = '';
             if (empty($contactData['contact_firstname'])) {
-                $contactErrors[] = 'Un prénom est requis';
+                $contactErrors[] = 'Un prénom est requis.';
             }
             if (empty($contactData['contact_lastname'])) {
-                $contactErrors[] = 'Un nom est requis';
+                $contactErrors[] = 'Un nom est requis.';
             }
             if (empty($contactData['contact_email'])) {
-                $contactErrors[] = 'Un email est requis';
+                $contactErrors[] = 'Un email est requis.';
             }
             if (!filter_var($contactData['contact_email'], FILTER_VALIDATE_EMAIL)) {
-                $contactErrors[] = 'Le format d\'email est invalide';
+                $contactErrors[] = 'Le format d\'email est invalide.';
             }
             if (empty($contactData['contact_phonenumber'])) {
-                $contactErrors[] = 'Un numéro de téléphone est requis';
+                $contactErrors[] = 'Un numéro de téléphone est requis.';
+            }
+            if (!is_numeric($contactData['contact_phonenumber'])) {
+                $contactErrors[] = 'Le numéro de téléphone doit être composé de chiffres uniquement.';
             }
             if (strlen($contactData['contact_phonenumber']) > 10) {
-                $contactErrors[] = 'Le numéro de téléphone doit être composé de dix chiffres seulement';
+                $contactErrors[] = 'Le numéro de téléphone doit être composé de dix chiffres seulement.';
             }
             if (strlen($contactData['contact_phonenumber']) < 10) {
-                $contactErrors[] = 'Le numéro de téléphone doit être composé de 10 chiffres';
+                $contactErrors[] = 'Le numéro de téléphone doit être composé de 10 chiffres.';
             }
             if (empty($contactData['contact_message'])) {
-                $contactErrors[] = 'Votre message ne peut être  vide';
-            } else {
-                $contactSuccess = "Bonjour " . htmlentities($contactData['contact_firstname']) . " " .
-                htmlentities($contactData['contact_lastname']) . ", votre message a bien été envoyé. Merci.";
+                $contactErrors[] = 'Votre message ne peut être vide.';
             }
-            return $this->twig->render('Contact/contact.html.twig', ['contactErrors' => $contactErrors, 'contactSuccess' => $contactSuccess]);
+            if (!empty($contactErrors)) {
+                return $this->twig->render('/Contact/index.html.twig', ['contactErrors' => $contactErrors]);
+            }
+            if (empty($contactErrors)) {
+                $contactSuccess = "success";
+                header('Location: /Contact/index/' . $contactSuccess);
+            }
         }
     }
 }
