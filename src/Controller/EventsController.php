63e69a9ddb29ae0c05d8c2a6704f693b5bdb0c09
Oleg Chenesseau <oleg.chenesseau@gmail.com>
2020-05-03 15:28:04 +0200

    Setting up the form validation

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -5,6 +5,8 @@ use App\Model\EventsManager;
 
 class EventsController extends AbstractController
 {
+    const MAX_TEXT = 255;
+
     /**
      * Display events page
      *
@@ -26,6 +28,72 @@ class EventsController extends AbstractController
         $eventsManager = new EventsManager();
         $types = $eventsManager->selectAllType();
 
-        return $this->twig->render('Events/add.html.twig', ['types' => $types]);
+        $errors = [];
+        $event = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $event = array_map('trim', $_POST);
+            $errors = $this->validateForm($event);
+
+            if (empty($errors)) {
+                header('Location: /events/admin');
+            }
+        }
+
+        return $this->twig->render('Events/add.html.twig', ['types' => $types,
+                                                                  'errors' => $errors,
+                                                                  'event' => $event]);
+    }
+
+    private function validateForm(array $data): array
+    {
+        $errors = array_merge($this->validateEmpty($data), $this->validateLength($data, self::MAX_TEXT));
+
+        return $errors;
+    }
+
+    private function validateLength(array $data, int $length): array
+    {
+        $errors = [];
+
+        if (strlen($data['title']) > $length) {
+            $errors[] = 'Veuillez indiquer un titre inférieur à ' . self::MAX_TEXT . ' caractères';
+        }
+
+        if (strlen($data['location']) > $length) {
+            $errors[] = 'Veuillez indiquer un lieu inférieur à ' . self::MAX_TEXT . ' caractères';
+        }
+
+        if (strlen($data['speaker_name']) > $length) {
+            $errors[] = 'Veuillez indiquer un nom inférieur à ' . self::MAX_TEXT . ' caractères';
+        }
+
+        return $errors;
+    }
+
+    private function validateEmpty(array $data): array
+    {
+        $errors = [];
+
+        if (empty($data['type'])) {
+            $errors[] = 'Veuillez choisir un type d\'événement';
+        }
+
+        if (empty($data['title'])) {
+            $errors[] = 'Veuillez indiquer le titre de l\'événement';
+        }
+
+        if (empty($data['date'])) {
+            $errors[] = 'Veuillez indiquer la date de l\'événement';
+        }
+
+        if (empty($data['time'])) {
+            $errors[] = 'Veuillez indiquer l\'horaire de l\'événement';
+        }
+
+        if (empty($data['location'])) {
+            $errors[] = 'Veuillez indiquer le lieu de l\'événement';
+        }
+
+        return $errors;
     }
 }
