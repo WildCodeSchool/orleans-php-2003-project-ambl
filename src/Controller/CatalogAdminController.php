7eda01d477e909346a00d975eb0a71405efd4ec2
dante18 <lablandinerie2@gmail.com>
2020-05-04 17:06:08 +0200

    Adding the catalogAdminAdd page

---
+++ src/Controller/CatalogAdminController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/10/17
+ * Time: 16:07
+ * PHP version 7
+ */
+
+namespace App\Controller;
+
+use App\Model\CatalogManager;
+use App\Model\ItemManager;
+
+/**
+ * Class CatalogAdminController
+ *
+ */
+class CatalogAdminController extends AbstractController
+{
+
+    /**
+     * Display catalogAdmin page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        $catalogManager = new CatalogManager();
+        $elements = $catalogManager->selectAll();
+
+        return $this->twig->render('CatalogAdmin/index.html.twig', ['elements' => $elements]);
+    }
+
+    public function add()
+    {
+        return $this->twig->render('CatalogAdmin/add.html.twig');
+    }
+}
