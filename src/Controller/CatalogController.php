271ef23e282e6463ee628e381284411ffa9ad58a
dante18 <lablandinerie2@gmail.com>
2020-05-08 17:51:05 +0200

    Search form management

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -24,9 +24,62 @@ class CatalogController extends AbstractController
      */
     public function index()
     {
-        $catalogManager = new CatalogManager();
-        $elements = $catalogManager->selectAll();
+        if (isset($_GET['search']) && !empty($_GET['search'])) {
+            $search = $_GET['search'];
+        } else {
+            $search = '';
+        }
 
-        return $this->twig->render('Catalog/index.html.twig', ['elements' => $elements]);
+        $catalogManager = new CatalogManager();
+        $elements = $catalogManager->selectAll($search);
+        $numberPageTotal = ceil($catalogManager->getNumberCatalogElement()/$catalogManager::MAX_RESULT);
+        $numberPage = 1;
+        $nextPage = 2;
+
+        return $this->twig->render('Catalog/index.html.twig', [
+            'elements' => $elements,
+            'numberPageTotal' => $numberPageTotal,
+            'numberPage' => $numberPage,
+            'nextPage' => $nextPage
+        ]);
+    }
+
+    /**
+     * Manage navigation from one page to another
+     *
+     * @param int $numberPage
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function page(int $numberPage): string
+    {
+        $catalogManager = new CatalogManager();
+        $numberPageTotal = ceil($catalogManager->getNumberCatalogElement()/$catalogManager::MAX_RESULT);
+
+        if ($numberPage <= 1) {
+            $elements = $catalogManager->selectAll();
+            $numberPage = 1;
+            $previousPage = 0;
+            $nextPage = 2;
+        } elseif ($numberPage > $numberPageTotal) {
+            $numberPage = $numberPageTotal;
+            $elements = $catalogManager->selectByPage($numberPage);
+            $previousPage = $numberPage - 1;
+            $nextPage = $numberPage + 1;
+        } else {
+            $elements = $catalogManager->selectByPage($numberPage);
+            $previousPage = $numberPage - 1;
+            $nextPage = $numberPage + 1;
+        }
+
+        return $this->twig->render('Catalog/index.html.twig', [
+            'elements' => $elements,
+            'numberPageTotal' => $numberPageTotal,
+            'numberPage' => $numberPage,
+            'previousPage' => $previousPage,
+            'nextPage' => $nextPage
+        ]);
     }
 }
--- src/Model/CatalogManager.php
+++ src/Model/CatalogManager.php
@@ -36,17 +36,36 @@ class CatalogManager extends AbstractManager
     /**
      * Get all row from database.
      *
+     * @param string $search
      * @return array
      */
-    public function selectAll(): array
+    public function selectAll(string $search = ''): array
     {
         $query = "SELECT " . self::TABLE . ".*, toxicity.name toxicity_name FROM " . self::TABLE . "
-                    JOIN toxicity ON toxicity.id=element.toxicity_id
-                    ORDER BY element.common_name LIMIT " . self::MAX_RESULT;
+                    JOIN toxicity ON toxicity.id=element.toxicity_id";
 
-        return $this->pdo->query($query)->fetchAll();
+        if ($search) {
+            $query .= " WHERE common_name LIKE :search ORDER BY element.common_name";
+        } else {
+            $query .= " ORDER BY element.common_name LIMIT " . self::MAX_RESULT;
+        }
+
+        $statement = $this->pdo->prepare($query);
+
+        if ($search) {
+            $statement->bindValue('search', $search . '%');
+        }
+
+        $statement->execute();
+
+        return $statement->fetchAll();
     }
 
+    /**
+     * Randomly retrieve a line
+     *
+     * @return array
+     */
     public function selectOneAtRandom(): array
     {
         $query = 'SELECT ' . self::TABLE . '.*, toxicity.name toxicity_name 
@@ -56,5 +75,34 @@ class CatalogManager extends AbstractManager
                   LIMIT 1';
 
         return $this->pdo->query($query)->fetch();
+    }
+
+    /**
+     * Retrieve the number of records in the table
+     *
+     * @return int
+     */
+    public function getNumberCatalogElement(): int
+    {
+        $query = 'SELECT id FROM ' . self::TABLE;
+        $statement = $this->pdo->query($query);
+
+        return $statement->rowCount();
+    }
+
+    /**
+     * Select an element group
+     *
+     * @param int $pageNumber
+     * @return array
+     */
+    public function selectByPage(int $pageNumber): array
+    {
+        $start = ($pageNumber - 1) * self::MAX_RESULT;
+        $query = "SELECT " . self::TABLE . ".*, toxicity.name toxicity_name FROM " . self::TABLE . "
+                    JOIN toxicity ON toxicity.id=element.toxicity_id
+                    ORDER BY element.common_name LIMIT " . $start . ' OFFSET ' . self::MAX_RESULT;
+
+        return $this->pdo->query($query)->fetchAll();
     }
 }
