bb242157fc39f44ae4a82b690f714a320d45e376
MonyaMya <you@example.com>
2020-05-11 16:46:04 +0200

    test

---
+++ src/Controller/LinkController.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+use App\Model\LinkManager;
+
+class LinkController extends AbstractController
+{
+
+    /**
+     * Display catalog page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        $linkManager = new LinkManager();
+        $links = $linkManager->selectAll();
+
+        return $this->twig->render('Links/index.html.twig', ['links' => $links]);
+    }
+}
