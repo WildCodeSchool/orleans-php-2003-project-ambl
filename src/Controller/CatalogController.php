064d5ed3ddd000053509f4dc96cf665c2bae1afe
dante18 <lablandinerie2@gmail.com>
2020-05-12 11:29:20 +0200

    Resolution of the error encountered when searching from one of the result pages.

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -55,33 +55,37 @@ class CatalogController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function page(int $numberPage): string
+    public function page($numberPage)
     {
-        $catalogManager = new CatalogManager();
-        $numberPageTotal = ceil($catalogManager->getNumberCatalogElement()/$catalogManager::MAX_RESULT);
-
-        if ($numberPage <= 1) {
-            $elements = $catalogManager->selectAll();
-            $numberPage = 1;
-            $previousPage = 0;
-            $nextPage = 2;
-        } elseif ($numberPage > $numberPageTotal) {
-            $numberPage = $numberPageTotal;
-            $elements = $catalogManager->selectByPage($numberPage);
-            $previousPage = $numberPage - 1;
-            $nextPage = $numberPage + 1;
+        if (isset($_GET['search']) && !empty($_GET['search'])) {
+            header('Location: /catalog/index/?search=' . $_GET['search']);
         } else {
-            $elements = $catalogManager->selectByPage($numberPage);
-            $previousPage = $numberPage - 1;
-            $nextPage = $numberPage + 1;
-        }
+            $catalogManager = new CatalogManager();
+            $numberPageTotal = ceil($catalogManager->getNumberCatalogElement()/$catalogManager::MAX_RESULT);
 
-        return $this->twig->render('Catalog/index.html.twig', [
-            'elements' => $elements,
-            'numberPageTotal' => $numberPageTotal,
-            'numberPage' => $numberPage,
-            'previousPage' => $previousPage,
-            'nextPage' => $nextPage
-        ]);
+            if ($numberPage <= 1) {
+                $elements = $catalogManager->selectAll('');
+                $numberPage = 1;
+                $previousPage = 0;
+                $nextPage = 2;
+            } elseif ($numberPage > $numberPageTotal) {
+                $numberPage = $numberPageTotal;
+                $elements = $catalogManager->selectByPage($numberPage);
+                $previousPage = $numberPage - 1;
+                $nextPage = $numberPage + 1;
+            } else {
+                $elements = $catalogManager->selectByPage($numberPage);
+                $previousPage = $numberPage - 1;
+                $nextPage = $numberPage + 1;
+            }
+
+            return $this->twig->render('Catalog/index.html.twig', [
+                'elements' => $elements,
+                'numberPageTotal' => $numberPageTotal,
+                'numberPage' => $numberPage,
+                'previousPage' => $previousPage,
+                'nextPage' => $nextPage
+            ]);
+        }
     }
 }
